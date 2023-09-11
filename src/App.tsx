eb6e3f89a2a6468d21de51340df8b46adf257853
YushiNakazato <yushi.nakazato@bloomscheme.com>
2023-09-11 10:23:10 +0900

    finished untill "React & Typescript"

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import ProjectsPage from './projects/ProjectsPage';
+import Hello from './Hello';
 
 function App() {
   return (
     <div className="container">
       <ProjectsPage />
+      <Hello name="David" enthusiasmLevel={3} />
     </div>
   )
 }
--- src/projects/Greeter.ts
+++
@@ -1,5 +0,0 @@
-function Greeter() {
-    return <h1>Hello</h1>
-}
-
-ReactDom.createRoot(document.getElementById('root')).render(<Greeter />);
