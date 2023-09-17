030835f45f6bcb9a40bc1330d38018eda662f650
YushiNakazato <yushi.nakazato@bloomscheme.com>
2023-09-17 21:35:43 +0900

    finished lab 19

--- src/projects/ProjectsPage.tsx
+++ src/projects/ProjectsPage.tsx
@@ -58,10 +58,19 @@ function ProjectsPage() {
   }, [currentPage]);
 
   const saveProject = (project: Project) => {
-    let updatedProjects = projects.map((p: Project) => {
-      return p.id === project.id ? project : p;
+    projectAPI
+    .put(project)
+    .then((updatedProject) => {
+      let updatedProjects = projects.map((p: Project) => {
+        return p.id === project.id ? new Project(updatedProject) : p;
       });
-    setProjects(updatedProjects);
+      setProjects(updatedProjects);
+    })
+    .catch((e) => {
+      if (e instanceof Error) {
+        setError(e.message);
+      }
+    });
   };
 
   return (
@@ -93,11 +102,11 @@ function ProjectsPage() {
           </div>
         </div>
       )}
-      
+
       {loading && (
         <div className="center-page">
           <span className="spinner primary"></span>
-+          <p>Loading...</p>
+          <p>Loading...</p>
         </div>
       )}
     </>
